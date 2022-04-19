8d149ac1aa2c304ba2fedb57801e4609e7c211c1
moonrailgun <moonrailgun@gmail.com>
2022-04-19 11:53:00 +0800

    feat: v1.2.0
    增加connect命令用于链接到节点网络

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import yargs from 'yargs';
+import { connectCommand } from './commands/connect';
 import { createCommand } from './commands/create';
 
 yargs
   .demandCommand()
   .command(createCommand)
+  .command(connectCommand)
   .alias('h', 'help')
   .scriptName('tailchat')
   .parse();
