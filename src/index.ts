33ab2dca142bf9bf656a9d98b0d0e3c57867b9cb
moonrailgun <moonrailgun@gmail.com>
2022-06-14 16:56:32 +0800

    feat: 增加插件类型声明命令

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import yargs from 'yargs';
-import { connectCommand } from './commands/connect';
 import { createCommand } from './commands/create';
+import { connectCommand } from './commands/connect';
+import { declarationCommand } from './commands/declaration';
 
 yargs
   .demandCommand()
   .command(createCommand)
   .command(connectCommand)
+  .command(declarationCommand)
   .alias('h', 'help')
   .scriptName('tailchat')
   .parse();
