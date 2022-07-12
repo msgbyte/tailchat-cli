83e4e67abcf3db406ad537a07730091ee4b2c53c
moonrailgun <moonrailgun@gmail.com>
2022-07-12 21:42:01 +0800

    chore: 优化错误提示

--- src/commands/declaration.ts
+++ src/commands/declaration.ts
@@ -44,15 +44,25 @@ export const declarationCommand: CommandModule = {
       content =
         "declare module '@capital/common';\ndeclare module '@capital/component';\n";
     } else if (source === 'github') {
-      const spinner = ora('正在从 Github 下载插件类型声明').start();
+      const spinner = ora('正在从 Github 下载插件类型声明');
+      const url =
+        'https://raw.githubusercontent.com/msgbyte/tailchat/master/web/tailchat.d.ts';
+      try {
+        spinner.start();
 
-      content = await got
-        .get(
-          'https://raw.githubusercontent.com/msgbyte/tailchat/master/web/tailchat.d.ts'
-        )
-        .then((res) => res.body);
+        content = await got.get(url).then((res) => res.body);
 
-      spinner.succeed('声明文件下载完毕');
+        spinner.succeed('声明文件下载完毕');
+      } catch (e) {
+        spinner.fail(
+          `声明下载失败, 可以直接通过浏览器访问最新类型定义: ${url}`
+        );
+        console.error(e);
+        return;
+      }
+    } else {
+      console.warn('未知的数据来源');
+      return;
     }
 
     if (content !== '') {
